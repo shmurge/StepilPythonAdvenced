4b9c6e67318d2fd7610b3666f8cd719cce8e3f69
Алексей Мишин <shmurge3@gmail.com>
2024-02-07 19:26:29 +0300

    info update #4.1

--- Chapter_2/task_2.2.7.py
+++ Chapter_2/task_2.2.7.py
@@ -11,3 +11,15 @@
 
 timur_choice = input()
 ruslan_choice = input()
+
+if timur_choice == ruslan_choice:
+    print("ничья")
+elif timur_choice == "ножницы" and ruslan_choice == "бумага":
+    print("Тимур")
+elif timur_choice == "бумага" and ruslan_choice == "камень":
+    print("Тимур")
+elif timur_choice == "камень" and ruslan_choice == "ножницы":
+    print("Тимур")
+else:
+    print("Руслан")
+
