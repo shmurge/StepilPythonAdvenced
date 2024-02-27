b3a12f3c5489d7caf54930d489dc0b350e5dd8b1
shmurge <shmurge3@gmail.com>
2024-02-27 19:03:32 +0300

    Info update #11.1

--- Chapter_4/task_4.5.4.py
+++ Chapter_4/task_4.5.4.py
@@ -7,3 +7,32 @@
 # Формат выходных данных
 # Программа должна вывести YES, если матрица симметрична относительно главной диагонали, и слово NO в противном случае.
 
+
+def create_matrix(size):
+    matrix = [input().split() for _ in range(size)]
+    for i in range(size):
+        for j in range(len(matrix[i])):
+            matrix[i][j] = int(matrix[i][j])
+
+    return matrix
+
+
+n = int(input())
+mat_1 = create_matrix(n)
+flag = True
+
+for i in range(len(mat_1)):
+    rows = mat_1[i]
+    cols = list()
+
+    for j in range(len(mat_1)):
+        cols.append(mat_1[j][i])
+
+    if rows != cols:
+        flag = False
+        break
+
+if flag:
+    print("YES")
+else:
+    print("NO")
