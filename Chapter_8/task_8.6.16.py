493a43e6315dc0a1607a42b87fada9d05d97902c
shmurge <shmurge3@gmail.com>
2024-04-03 18:02:35 +0300

    Info update #18.1

--- Chapter_8/task_8.6.16.py
+++ Chapter_8/task_8.6.16.py
@@ -8,3 +8,22 @@
 # Программа должна вывести цифры в соответствии с условием задачи. Если общих цифр нет, то ничего выводить не нужно.
 
 n = int(input())
+nums_ls = list()
+result_set = set()
+temp_set = set()
+
+for i in range(n):
+    str_num = input()
+    nums_ls.append(str_num)
+    if i == 0:
+        for j in range(len(str_num)):
+            result_set.update(str_num[j])
+
+for i in range(1, len(nums_ls)):
+    for j in range(len(nums_ls[i])):
+        temp_set.update(nums_ls[i][j])
+    result_set.intersection_update(temp_set)
+    temp_set.clear()
+
+result_set = sorted(result_set)
+print(*result_set)
