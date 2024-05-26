b4e7448743d3ab74340b94bbb824f22753a098be
Алексей Мишин <shmurge3@gmail.com>
2024-05-26 15:38:14 +0300

    info update #32

---
+++ Chapter_16/task_16.3.1.py
@@ -0,0 +1,31 @@
+# Напишите функцию concat(), принимающую переменное количество аргументов
+# и объединяющую их в одну строку через разделитель (sep). Если разделитель не задан, им служит пробел.
+
+# Примечание 1. Обратите внимание, что функция concat() должна принимать не список,
+# а именно переменное количество аргументов.
+
+# Примечание 2. Приведенный ниже код, при условии, что функция concat() написана правильно
+
+# print(concat('hello', 'python', 'and', 'stepik'))
+# print(concat('hello', 'python', 'and', 'stepik', sep='*'))
+# print(concat('hello', 'python', sep='()()()'))
+# print(concat('hello', sep='()'))
+# print(concat(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='$$'))
+# должен выводить:
+
+# hello python and stepik
+# hello*python*and*stepik
+# hello()()()python
+# hello
+# 1$$2$$3$$4$$5$$6$$7$$8$$9
+# Примечание 3. Вызывать функцию concat() не нужно, требуется только реализовать.
+
+def concat(*args, sep=' '):
+    ls = []
+    for i in args:
+        ls.append(str(i))
+    s = f'{sep}'.join(ls)
+    return s
+
+
+print(concat(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='$$'))
